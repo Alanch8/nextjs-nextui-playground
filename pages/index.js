b903496355eafc763c26317c341166cfe07e08ca
Aitor Lancharro <aitor.lancharro@gmail.com>
2022-09-13 13:42:23 +0200

    some tests

--- pages/index.js
+++ pages/index.js
@@ -66,12 +66,7 @@ export default function Home() {
           <a
             href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          ></a>
         </div>
       </main>
 
